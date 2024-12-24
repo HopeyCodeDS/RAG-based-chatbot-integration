bf384fc37d8ecc673fa1a90488e8e54f974f9a7c
Momodu_Ope <opeyemi.momodu@student.kdg.be>
2024-12-24 15:05:25 +0100

    created a function to load document from the data folder

---
+++ populate_database.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import shutil
+import time
+from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+from get_embedding_function import get_embedding_function
+from langchain_chroma import Chroma
+import chromadb
+
+CHROMA_PATH = "chroma"
+DATA_PATH = "data"
+
+
+def load_documents():
+    print("📚 Loading documents from:", DATA_PATH)
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    documents = document_loader.load()
+
+    print("\nLoaded files:")
+    sources = set()
+    for doc in documents:
+        source = doc.metadata.get("source", "Unknown")
+        if source not in sources:
+            sources.add(source)
+            print(f"- {source}")
+    print(f"\nTotal documents loaded: {len(documents)}")
+
+    return documents
+
+
